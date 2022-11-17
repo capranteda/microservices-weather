6d10c08c9711d594b2272e37312e12a7ee15217d
capranteda <capranteda@gmail.com>
2022-11-17 14:29:00 -0300

    Add data access to precipitation service

---
+++ Cloudweather.Precipitation/DataAccess/Precipitation.cs
@@ -0,0 +1,10 @@
+﻿namespace Cloudweather.Precipitation.DataAccess;
+
+public class Precipitation
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public string WeatherTipe { get; set; }
+    public string ZipCode { get; set; }
+}
---
+++ Cloudweather.Precipitation/DataAccess/PreciptDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cloudweather.Precipitation.DataAccess;
+
+public class PreciptDbContext: DbContext
+{
+    public PreciptDbContext()
+    {
+    }
+    
+    public PreciptDbContext(DbContextOptions<PreciptDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Precipitation> Precipitation { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Precipitation>(b=> b.ToTable("precipitation"));
+    }
+    
+    
+}
