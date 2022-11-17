3c9e99841b92e9ebc05dad521a2b70896d24d422
capranteda <capranteda@gmail.com>
2022-11-17 15:37:30 -0300

    Add data access to temperature and report services

---
+++ Cloudweather.Report/DataAccess/WeatherReportDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cloudweather.Report.DataAccess;
+
+public class WeatherReportDbContext: DbContext
+{
+    public WeatherReportDbContext()
+    {
+    }
+    
+    public WeatherReportDbContext(DbContextOptions<WeatherReportDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<WeatherReport> WeatherReport { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WeatherReport>(b=> b.ToTable("weatherreport"));
+    }
+    
+}
---
+++ Cloudweather.Temperature/DataAccess/TemperatureDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cloudweather.Temperature.DataAccess;
+
+public class TemperatureDbContext : DbContext
+{
+    public TemperatureDbContext()
+    {
+    }
+    
+    public TemperatureDbContext(DbContextOptions<TemperatureDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Temperature> Temperature { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Temperature>(b=> b.ToTable("temperature"));
+    }
+}
